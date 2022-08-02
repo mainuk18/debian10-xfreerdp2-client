c2a4374aab3882e72a975a40e2dd171e34036ba6
mainuk18 <105724505+mainuk18@users.noreply.github.com>
2022-08-02 18:29:25 +0400

    Update install.sh
    wget for xfreerdp.sh

--- install.sh
+++ install.sh
@@ -3,35 +3,7 @@ apt update && apt full-upgrade -y && apt install xorg slim freerdp2-x11 ufw -y
 echo "default_user	user" >> /etc/slim.conf
 echo "auto_login	yes"  >> /etc/slim.conf
 
-/usr/bin/sh -c "cat > /home/user/xfreerdp2.sh <<EOF
-#!/usr/bin/sh
-while :
-do
-    if [[ $(pgrep xfreerdp) == ""  ]]
-    then
-        xfreerdp \
-          +aero \
-          +auto-reconnect \
-          /auto-reconnect-max-retries:0 \
-          /cert-ignore \
-          -compression \
-          /d:dc \
-          -decorations \
-          /dynamic-resolution \
-          /f \
-          /rfx \
-          /video \
-          -sec-nla \
-          /sec:tls \
-          /rfx-mode:image \
-          /u:'' \
-          /v:192.168.100.3:3389
-
-# -r scard:"EZCCID Smart Card Reader 00 00"="EZCCID Smart Card Reader"
-    fi
-done
-EOF
-"
+wget https://raw.githubusercontent.com/mainuk18/debian10-xfreerdp2-client/main/xfreerdp2.sh -P /home/user/
 
 chmod +x /home/user/xfreerdp2.sh
 
