7e3ddbb87b20de7e8d01a32e79c961dcb0092278
mainuk18 <105724505+mainuk18@users.noreply.github.com>
2022-08-02 18:34:12 +0400

    Update xfreerdp2.sh

--- xfreerdp2.sh
+++ xfreerdp2.sh
@@ -1,25 +1,25 @@
 while :
 do
-    if [[ $(pgrep rdesktop) == ""  ]]
-    then
+if [[ $(pgrep xfreerdp) == ""  ]]
+then
 #       rdesktop -f -u '' -d dc -N -b -k en-us -a 24 -x l -r sound:local:alsa:default s3
-        xfreerdp \
-          +aero \
-          +auto-reconnect \
-          /auto-reconnect-max-retries:0 \
-          /cert-ignore \
-          -compression \
-          /d:dc \
-          -decorations \
-          /dynamic-resolution \
-          /f \
-          /rfx \
-          /video \
-          -sec-nla \
-          /sec:tls \
-          /rfx-mode:image \
-          /u:'' \
-          /v:192.168.100.3:3389
-# -r scard:"EZCCID Smart Card Reader 00 00"="EZCCID Smart Card Reader"
-    fi
+xfreerdp \
++aero \
++auto-reconnect \
+/auto-reconnect-max-retries:0 \
+/cert-ignore \
+-compression \
+/d:dc \
+-decorations \
+/dynamic-resolution \
+/f \
+/rfx \
+/video \
+-sec-nla \
+/sec:tls \
+/rfx-mode:image \
+/u:'' \
+/v:192.168.100.3:3389
+
+fi
 done
