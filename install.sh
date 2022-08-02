c66aea0ca2f9c704ad562e7b70777ff8ef454409
mainuk18 <105724505+mainuk18@users.noreply.github.com>
2022-08-02 17:08:44 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt update && apt full-upgrade -y && apt install xorg slim freerd2-x11 ufw -y
+apt update && apt full-upgrade -y && apt install xorg slim freerdp2-x11 ufw -y
 
 echo "default_user	user" >> /etc/slim.conf
 echo "auto_login	yes"  >> /etc/slim.conf
